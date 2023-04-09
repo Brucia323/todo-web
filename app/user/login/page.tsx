c421ae9c71f7455c7659c186ce3edb2ed25f0e5a
周晨阳 <1448517833@qq.com>
2023-04-09 15:43:12 +0800

    Create page.tsx

---
+++ app/user/login/page.tsx
@@ -0,0 +1,58 @@
+'use client';
+
+import { PASSWORD } from '@/lib/utils';
+import { Button, PasswordInput, Stack, TextInput } from '@mantine/core';
+import { isEmail, matches, useForm } from '@mantine/form';
+import { IconMail, IconPassword } from '@tabler/icons-react';
+import React from 'react';
+
+interface FormValues {
+  email: string;
+  password: string;
+}
+
+const Login: React.FC = () => {
+  const form = useForm<FormValues>({
+    validate: {
+      email: isEmail('邮箱无效'),
+      password: matches(PASSWORD, '密码无效'),
+    },
+  });
+
+  const handleSubmit = async (
+    values: FormValues,
+    _event: React.FormEvent<HTMLFormElement>
+  ) => {
+    const response = await fetch('/api/login', {
+      method: 'POST',
+      body: JSON.stringify(values),
+    });
+    if (response.status === 201) {
+      console.log('login');
+    }
+  };
+
+  return (
+    <Stack align="center" justify="center" h="100%">
+      <form onSubmit={form.onSubmit(handleSubmit)}>
+        <Stack spacing="lg">
+          <TextInput
+            placeholder="邮箱"
+            icon={<IconMail />}
+            {...form.getInputProps('email')}
+          />
+          <PasswordInput
+            placeholder="密码"
+            icon={<IconPassword />}
+            {...form.getInputProps('password')}
+          />
+          <Button fullWidth type="submit">
+            登录
+          </Button>
+        </Stack>
+      </form>
+    </Stack>
+  );
+};
+
+export default Login;
