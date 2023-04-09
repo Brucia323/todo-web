12d61abcbf59ade67bf9227bbcde1ad7df5c5ae4
周晨阳 <1448517833@qq.com>
2023-04-09 15:23:15 +0800

    Create emotion.tsx

---
+++ app/emotion.tsx
@@ -0,0 +1,34 @@
+'use client';
+
+import { CacheProvider } from '@emotion/react';
+import {
+  MantineProvider,
+  MantineProviderProps,
+  useEmotionCache,
+} from '@mantine/core';
+import { useServerInsertedHTML } from 'next/navigation';
+import React from 'react';
+
+const RootStyleRegistry: React.FC<MantineProviderProps> = ({ children }) => {
+  const cache = useEmotionCache();
+  cache.compat = true;
+
+  useServerInsertedHTML(() => (
+    <style
+      data-emotion={`${cache.key} ${Object.keys(cache.inserted).join(' ')}`}
+      dangerouslySetInnerHTML={{
+        __html: Object.values(cache.inserted).join(' '),
+      }}
+    />
+  ));
+
+  return (
+    <CacheProvider value={cache}>
+      <MantineProvider withGlobalStyles withNormalizeCSS>
+        {children}
+      </MantineProvider>
+    </CacheProvider>
+  );
+};
+
+export default RootStyleRegistry;
