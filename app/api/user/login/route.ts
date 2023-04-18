58348851ae8ae9d57f3c37cb1cb113af233dfc57
ZHOU CHENYANG <zhouchenyang@infuturespace.com>
2023-04-18 18:08:20 +0800

    Create route.ts

---
+++ app/api/user/login/route.ts
@@ -0,0 +1,10 @@
+import { FETCH } from "@/lib/utils";
+
+export async function POST(request: Request) {
+  const body = await request.json();
+  return await fetch('http://localhost:8080/login', {
+    method: FETCH.METHOD.POST,
+    headers: FETCH.HEADER,
+    body: JSON.stringify(body),
+  });
+}
