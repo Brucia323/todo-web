86c7e5d978263342945d275caade88480bf0c17e
ZHOU CHENYANG <zhouchenyang@infuturespace.com>
2023-04-18 18:08:46 +0800

    Update utils.ts

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,8 +1,13 @@
 export const PASSWORD = /^[a-zA-Z]\w{5,17}$/;
 
-export const FETCH_METHOD = {
-  GET: 'GET',
-  POST: 'POST',
-  PUT: 'PUT',
-  DELETE: 'DELETE'
-}
+export const FETCH = {
+  METHOD: {
+    GET: 'GET',
+    POST: 'POST',
+    PUT: 'PUT',
+    DELETE: 'DELETE',
+  },
+  HEADER: {
+    'Content-Type': 'application/json',
+  },
+};
