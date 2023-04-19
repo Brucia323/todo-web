c175641c35ea54da4b197b5b37e04dfc14102c3f
ZHOU CHENYANG <zhouchenyang@infuturespace.com>
2023-04-19 17:59:47 +0800

    Create page.tsx

---
+++ app/todo/dashboard/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import { Button, Container } from '@mantine/core';
+
+export default function Dashboard() {
+  return (
+    <Container>
+      <Button>test</Button>
+    </Container>
+  );
+}
