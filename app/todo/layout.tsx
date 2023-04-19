30bbd3411576ea4e4380085bfa5eaef157d9ad28
ZHOU CHENYANG <zhouchenyang@infuturespace.com>
2023-04-19 17:59:51 +0800

    Update layout.tsx

--- app/todo/layout.tsx
+++ app/todo/layout.tsx
@@ -1,20 +1,49 @@
 'use client';
 
-import { ActionIcon, AppShell, Flex, Header, Navbar } from '@mantine/core';
+import {
+  AppShell,
+  Flex,
+  Header,
+  Menu,
+  Navbar,
+  UnstyledButton,
+} from '@mantine/core';
+import { useLocalStorage, useSessionStorage } from '@mantine/hooks';
+import { notifications } from '@mantine/notifications';
+import { useRouter } from 'next/navigation';
 import React from 'react';
 
 export default function Layout({ children }: { children: React.ReactNode }) {
+  const router = useRouter();
+  const [value, setValue] = useSessionStorage({
+    key: 'token',
+    getInitialValueInEffect: false,
+  });
+
+  if (!value) {
+    notifications.show({ title: '未登录', message: '请先登录', color: 'red' });
+    router.push('/');
+  }
+
+  const user: { name: string; token: string } = JSON.parse(value);
+
   return (
     <AppShell
       padding="md"
       navbar={
-        <Navbar width={{ base: 300 }} height={500} p="xs">
+        <Navbar width={{ base: 300 }} height="100%" p="xs">
           Todo
         </Navbar>
       }
       header={
         <Header height={60} p="xs">
-          Todo
+          <Flex justify="flex-end" align="center">
+            <Menu shadow="md" width={200}>
+              <Menu.Target>
+                <UnstyledButton>{user.name}</UnstyledButton>
+              </Menu.Target>
+            </Menu>
+          </Flex>
         </Header>
       }
     >
