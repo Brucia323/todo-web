eab9e193b1c1799523afc32466d3a975558b524c
ZHOU CHENYANG <zhouchenyang@infuturespace.com>
2023-04-21 18:01:38 +0800

    Update route.ts

--- app/api/todo/route.ts
+++ app/api/todo/route.ts
@@ -1,4 +1,4 @@
-import { FETCH } from '@/lib/utils';
+import { HTTP_METHODS } from 'next/dist/server/web/http';
 import { redirect } from 'next/navigation';
 
 export async function GET(request: Request) {
@@ -7,7 +7,7 @@ export async function GET(request: Request) {
     redirect('/');
   }
   return await fetch('http://localhost:8080/todo', {
-    method: FETCH.METHOD.GET,
+    method: HTTP_METHODS[0],
     headers: {
       Authorization: authorization,
     },
@@ -21,10 +21,26 @@ export async function POST(request: Request) {
   }
   const body = await request.json();
   return await fetch('http://localhost:8080/todo', {
-    method: FETCH.METHOD.POST,
+    method: HTTP_METHODS[3],
     headers: {
+      'Content-Type': 'application/json',
       Authorization: authorization,
-      ...FETCH.HEADER,
+    },
+    body: JSON.stringify(body),
+  });
+}
+
+export async function PUT(request: Request) {
+  const authorization = request.headers.get('Authorization');
+  if (authorization === null) {
+    redirect('/');
+  }
+  const body = await request.json();
+  return await fetch(`http://localhost:8080/todo/${body.id}`, {
+    method: HTTP_METHODS[4],
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: authorization,
     },
     body: JSON.stringify(body),
   });
