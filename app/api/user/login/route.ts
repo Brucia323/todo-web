512d526d764f7894d4fd1f357afbdc661f60242b
ZHOU CHENYANG <zhouchenyang@infuturespace.com>
2023-04-21 18:01:41 +0800

    Update route.ts

--- app/api/user/login/route.ts
+++ app/api/user/login/route.ts
@@ -1,10 +1,10 @@
-import { FETCH } from "@/lib/utils";
+import { HTTP_METHODS } from 'next/dist/server/web/http';
 
 export async function POST(request: Request) {
   const body = await request.json();
   return await fetch('http://localhost:8080/login', {
-    method: FETCH.METHOD.POST,
-    headers: FETCH.HEADER,
+    method: HTTP_METHODS[3],
+    headers: { 'Content-Type': 'application/json' },
     body: JSON.stringify(body),
   });
 }
