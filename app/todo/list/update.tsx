3fe31ec6eb44280627a5cbd400e3f3b4cc12c701
ZHOU CHENYANG <zhouchenyang@infuturespace.com>
2023-04-23 14:07:28 +0800

    Update update.tsx

--- app/todo/list/update.tsx
+++ app/todo/list/update.tsx
@@ -1,23 +1,39 @@
-import { useDisclosure } from '@mantine/hooks';
+import { useDisclosure, useSessionStorage } from '@mantine/hooks';
 import { Dialog, Group, Button, Text, NumberInput } from '@mantine/core';
 import { useState } from 'react';
 import { HTTP_METHODS } from 'next/dist/server/web/http';
+import { UserType } from '@/lib/types';
+import { notifications } from '@mantine/notifications';
 
 interface UpdateProps {
   min: number;
   max: number;
+  id: number;
 }
 
-export default function Update({ min, max }: UpdateProps) {
+export default function Update({ min, max, id }: UpdateProps) {
   const [value, setValue] = useState<number | ''>(min);
   const [loading, setLoading] = useState(false);
   const [opened, { toggle, close }] = useDisclosure(false);
+  const [user] = useSessionStorage<UserType>({ key: 'user' });
 
   const handleClick = async () => {
     try {
       setLoading(true);
-      const response = await fetch('/api/todo', { method: HTTP_METHODS[4] });
+      const response = await fetch(`/api/todo/${id}`, {
+        method: HTTP_METHODS[3],
+        headers: {
+          Authorization: user.token,
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ currentAmount: value }),
+      });
+      if (response.status === 201) {
+        notifications.show({ message: '更新成功', color: 'green' });
+        close();
+      }
     } catch {
+      notifications.show({ message: '更新失败', color: 'red' });
     } finally {
       setLoading(false);
     }
@@ -50,7 +66,7 @@ export default function Update({ min, max }: UpdateProps) {
             type="number"
             onChange={setValue}
           />
-          <Button onClick={close} loading={loading}>
+          <Button onClick={handleClick} loading={loading}>
             提交
           </Button>
         </Group>
