43e450584f951aca922aa36afbbc813571aa6bb7
ZHOU CHENYANG <zhouchenyang@infuturespace.com>
2023-04-24 10:51:26 +0800

    Update emotion.tsx

--- app/emotion.tsx
+++ app/emotion.tsx
@@ -31,7 +31,7 @@ export default function RootStyleRegistry({
       <MantineProvider
         withGlobalStyles
         withNormalizeCSS
-        theme={{ colorScheme: 'dark' }}
+        theme={{ colorScheme: 'light' }}
       >
         <ModalsProvider labels={{ confirm: '确认', cancel: '取消' }}>
           <DatesProvider settings={{ locale: 'zh-cn' }}>
