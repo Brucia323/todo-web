0997200e580a3bcb809bd0a2bbd325b5b259a1be
ZHOU CHENYANG <zhouchenyang@infuturespace.com>
2023-04-25 16:25:54 +0800

    Update page.tsx

--- app/todo/dashboard/page.tsx
+++ app/todo/dashboard/page.tsx
@@ -1,10 +1,20 @@
 'use client';
 
 import { UserType } from '@/lib/types';
-import { Card, Container, Text, Title } from '@mantine/core';
+import { Card, Container, Stack } from '@mantine/core';
 import { useSessionStorage } from '@mantine/hooks';
 import { HTTP_METHODS } from 'next/dist/server/web/http';
 import useSWR from 'swr';
+import {
+  Area,
+  AreaChart,
+  CartesianGrid,
+  Legend,
+  ResponsiveContainer,
+  Tooltip,
+  XAxis,
+  YAxis,
+} from 'recharts';
 
 interface EfficiencyType {
   amount: number;
@@ -26,14 +36,63 @@ export default function Dashboard() {
   const [value] = useSessionStorage<UserType | undefined>({ key: 'user' });
   const { data } = useSWR(
     value ? ['/api/todo/efficiency', value.token] : null,
-    ([input, token]) => fetcher(input, token),
+    ([input, token]) => fetcher(input, token)
   );
 
   return (
     <Container>
-      <Card shadow="sm" padding="lg" radius="md" withBorder>
-        <Title>效率指数</Title>
-      </Card>
+      <Stack>
+        <Card shadow="md">
+          <ResponsiveContainer width="100%" height={320}>
+            <AreaChart height={250} data={data}>
+              <defs>
+                <linearGradient id="amount" x1="0" y1="0" x2="0" y2="1">
+                  <stop offset="5%" stopColor="#228BE6" stopOpacity={0.4} />
+                  <stop offset="95%" stopColor="#228BE6" stopOpacity={0} />
+                </linearGradient>
+              </defs>
+              <XAxis
+                dataKey="time"
+                axisLine={false}
+                tickLine={false}
+                padding={{ left: 10, right: 10 }}
+                minTickGap={5}
+                style={{
+                  fontSize: '12px',
+                  fontFamily: 'Inter; Helvetica',
+                  color: 'red',
+                }}
+                tick={{ transform: 'translate(0, 6)' }}
+              />
+              <YAxis
+                axisLine={false}
+                tickLine={false}
+                tick={{ transform: 'translate(-3, 0)' }}
+                style={{
+                  fontSize: '12px',
+                  fontFamily: 'Inter; Helvetica',
+                }}
+              />
+              <CartesianGrid
+                strokeDasharray="3 3"
+                horizontal={true}
+                vertical={false}
+              />
+              <Tooltip wrapperStyle={{ outline: 'none' }} />
+              <Legend verticalAlign="top" />
+              <Area
+                type="linear"
+                dataKey="amount"
+                name="效率指数"
+                fillOpacity={1}
+                fill="url(#amount)"
+                strokeWidth={2}
+                stroke="#228BE6"
+              />
+            </AreaChart>
+          </ResponsiveContainer>
+        </Card>
+      </Stack>
     </Container>
   );
 }
