915cbd6dd99b3d4df546d5bf6d4a581a8b063528
ZHOU CHENYANG <zhouchenyang@infuturespace.com>
2023-04-26 15:07:47 +0800

    Update page.tsx

--- app/todo/dashboard/page.tsx
+++ app/todo/dashboard/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { UserType } from '@/lib/types';
-import { Card, Container, Stack } from '@mantine/core';
+import { Card, Container, Stack, Title } from '@mantine/core';
 import { useSessionStorage } from '@mantine/hooks';
 import { HTTP_METHODS } from 'next/dist/server/web/http';
 import useSWR from 'swr';
@@ -42,55 +42,58 @@ export default function Dashboard() {
   return (
     <Container>
       <Stack>
-        <Card shadow="md">
-          <ResponsiveContainer width="100%" height={320}>
-            <AreaChart data={data}>
-              <defs>
-                <linearGradient id="amount" x1="0" y1="0" x2="0" y2="1">
-                  <stop offset="5%" stopColor="#228BE6" stopOpacity={0.4} />
-                  <stop offset="95%" stopColor="#228BE6" stopOpacity={0} />
-                </linearGradient>
-              </defs>
-              <XAxis
-                dataKey="time"
-                axisLine={false}
-                tickLine={false}
-                padding={{ left: 10, right: 10 }}
-                minTickGap={5}
-                style={{
-                  fontSize: '12px',
-                  fontFamily: 'Inter; Helvetica',
-                  color: 'red',
-                }}
-                tick={{ transform: 'translate(0, 6)' }}
-              />
-              <YAxis
-                axisLine={false}
-                tickLine={false}
-                tick={{ transform: 'translate(-3, 0)' }}
-                style={{
-                  fontSize: '12px',
-                  fontFamily: 'Inter; Helvetica',
-                }}
-              />
-              <CartesianGrid
-                strokeDasharray="3 3"
-                horizontal={true}
-                vertical={false}
-              />
-              <Tooltip wrapperStyle={{ outline: 'none' }} />
-              <Legend verticalAlign="top" />
-              <Area
-                type="linear"
-                dataKey="amount"
-                name="效率指数"
-                fillOpacity={1}
-                fill="url(#amount)"
-                strokeWidth={2}
-                stroke="#228BE6"
-              />
-            </AreaChart>
-          </ResponsiveContainer>
+        <Card shadow="md" withBorder>
+          <Stack>
+            <Title>效率指数</Title>
+            <ResponsiveContainer width="100%" height={320}>
+              <AreaChart data={data}>
+                <defs>
+                  <linearGradient id="amount" x1="0" y1="0" x2="0" y2="1">
+                    <stop offset="5%" stopColor="#228BE6" stopOpacity={0.4} />
+                    <stop offset="95%" stopColor="#228BE6" stopOpacity={0} />
+                  </linearGradient>
+                </defs>
+                <XAxis
+                  dataKey="time"
+                  axisLine={false}
+                  tickLine={false}
+                  padding={{ left: 10, right: 10 }}
+                  minTickGap={5}
+                  style={{
+                    fontSize: '12px',
+                    fontFamily: 'Inter; Helvetica',
+                    color: 'red',
+                  }}
+                  tick={{ transform: 'translate(0, 6)' }}
+                />
+                <YAxis
+                  axisLine={false}
+                  tickLine={false}
+                  tick={{ transform: 'translate(-3, 0)' }}
+                  style={{
+                    fontSize: '12px',
+                    fontFamily: 'Inter; Helvetica',
+                  }}
+                />
+                <CartesianGrid
+                  strokeDasharray="3 3"
+                  horizontal={true}
+                  vertical={false}
+                />
+                <Tooltip wrapperStyle={{ outline: 'none' }} />
+                <Legend verticalAlign="top" />
+                <Area
+                  type="linear"
+                  dataKey="amount"
+                  name="效率指数"
+                  fillOpacity={1}
+                  fill="url(#amount)"
+                  strokeWidth={2}
+                  stroke="#228BE6"
+                />
+              </AreaChart>
+            </ResponsiveContainer>
+          </Stack>
         </Card>
       </Stack>
     </Container>
