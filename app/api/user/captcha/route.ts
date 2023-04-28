2a7c9dc71f89c1f85d8d9a1b81f0c951584ac572
ZHOU CHENYANG <zhouchenyang@infuturespace.com>
2023-04-28 16:54:15 +0800

    Create route.ts

---
+++ app/api/user/captcha/route.ts
@@ -0,0 +1,7 @@
+import { HTTP_METHODS } from 'next/dist/server/web/http';
+
+export async function GET(_request: Request) {
+  return await fetch('http://localhost:8080/user/captcha', {
+    method: HTTP_METHODS[0],
+  });
+}
