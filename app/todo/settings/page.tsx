0986dbf7a5c3974e2caceb2cb0167a1142488f46
周晨阳 <1448517833@qq.com>
2023-04-29 16:21:18 +0800

    Update page.tsx

--- app/todo/settings/page.tsx
+++ app/todo/settings/page.tsx
@@ -1,31 +1,7 @@
 'use client';
 
-import {
-  Container,
-  Stack,
-  Tabs,
-  Title,
-} from '@mantine/core';
-import Time from './time';
+import { Title } from '@mantine/core';
 
 export default function Settings() {
-  return (
-    <Container>
-      <Stack>
-        <Title>设置</Title>
-        <Tabs defaultValue="time">
-          <Tabs.List>
-            <Tabs.Tab value="time">你的时间</Tabs.Tab>
-            <Tabs.Tab value="personal">个人设置</Tabs.Tab>
-          </Tabs.List>
-          <Tabs.Panel value="time">
-            <Time />
-          </Tabs.Panel>
-          <Tabs.Panel value="personal" pt="xs">
-            Messages tab content
-          </Tabs.Panel>
-        </Tabs>
-      </Stack>
-    </Container>
-  );
+  return <Title>设置</Title>;
 }
