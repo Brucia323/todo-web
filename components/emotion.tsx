0d02ef189c6718cf6f45c836255901d0dee7c57d
周晨阳 <1448517833@qq.com>
2023-04-29 16:21:43 +0800

    Create emotion.tsx

---
+++ components/emotion.tsx
@@ -0,0 +1,45 @@
+'use client';
+
+import { CacheProvider } from '@emotion/react';
+import { MantineProvider, useEmotionCache } from '@mantine/core';
+import { useServerInsertedHTML } from 'next/navigation';
+import React from 'react';
+import { Notifications } from '@mantine/notifications';
+import { ModalsProvider } from '@mantine/modals';
+import { DatesProvider } from '@mantine/dates';
+import 'dayjs/locale/zh-cn';
+
+export default function RootStyleRegistry({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const cache = useEmotionCache();
+  cache.compat = true;
+
+  useServerInsertedHTML(() => (
+    <style
+      data-emotion={`${cache.key} ${Object.keys(cache.inserted).join(' ')}`}
+      dangerouslySetInnerHTML={{
+        __html: Object.values(cache.inserted).join(' '),
+      }}
+    />
+  ));
+
+  return (
+    <CacheProvider value={cache}>
+      <MantineProvider
+        withGlobalStyles
+        withNormalizeCSS
+        theme={{ defaultRadius: 'md' }}
+      >
+        <ModalsProvider labels={{ confirm: '确认', cancel: '取消' }}>
+          <DatesProvider settings={{ locale: 'zh-cn' }}>
+            {children}
+          </DatesProvider>
+        </ModalsProvider>
+        <Notifications />
+      </MantineProvider>
+    </CacheProvider>
+  );
+}
