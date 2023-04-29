819fc65a869645c4e52c5cf281d85b46ddf18af8
周晨阳 <1448517833@qq.com>
2023-04-29 16:21:47 +0800

    Create logo.tsx

---
+++ components/logo.tsx
@@ -0,0 +1,16 @@
+import { Text } from '@mantine/core';
+
+export default function Logo() {
+  return (
+    <Text
+      variant="gradient"
+      gradient={{ from: 'indigo', to: 'cyan', deg: 45 }}
+      sx={{ fontFamily: 'Greycliff CF, sans-serif', cursor: 'default' }}
+      ta="center"
+      fz="xl"
+      fw={700}
+    >
+      Todo
+    </Text>
+  );
+}
