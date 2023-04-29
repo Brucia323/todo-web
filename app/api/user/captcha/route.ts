f39d4b0d1fe5c6199a3d37636f93ecfe217b8f08
周晨阳 <1448517833@qq.com>
2023-04-29 17:31:43 +0800

    Update route.ts

--- app/api/user/captcha/route.ts
+++ app/api/user/captcha/route.ts
@@ -1,6 +1,6 @@
 import { HTTP_METHODS } from 'next/dist/server/web/http';
 
-export async function GET() {
+export async function GET(request: Request) {
   return await fetch('http://localhost:8080/user/captcha', {
     method: HTTP_METHODS[0],
   });
