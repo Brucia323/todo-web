0d9539613dba2244b91ceafc49073af0bf7bad61
周晨阳 <1448517833@qq.com>
2023-05-04 18:22:02 +0800

    Update page.tsx

--- app/todo/settings/@personal/page.tsx
+++ app/todo/settings/@personal/page.tsx
@@ -1,7 +1,19 @@
 'use client';
 
-import { Stack } from '@mantine/core';
+import { Button, Card, Divider, Group, Stack, Title } from '@mantine/core';
 
 export default function Personal() {
-  return <Stack></Stack>;
+  return (
+    <Stack>
+      <Card shadow="md" withBorder>
+        <Stack>
+          <Title>修改密码</Title>
+          <Divider />
+          <Group>
+            <Button>修改密码</Button>
+          </Group>
+        </Stack>
+      </Card>
+    </Stack>
+  );
 }
