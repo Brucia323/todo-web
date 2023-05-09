b41e232e463fe1d0ca197ba11b27225ef6a95fb7
周晨阳 <1448517833@qq.com>
2023-05-09 21:16:35 +0800

    Update layout.tsx

--- app/todo/dashboard/layout.tsx
+++ app/todo/dashboard/layout.tsx
@@ -6,15 +6,18 @@ import React from 'react';
 export default function Layout({
   children,
   efficiency,
+  program,
 }: {
   children: React.ReactNode;
   efficiency: React.ReactNode;
+  program: React.ReactNode;
 }) {
   return (
     <Container>
       <Stack>
         {children}
         {efficiency}
+        {program}
       </Stack>
     </Container>
   );
